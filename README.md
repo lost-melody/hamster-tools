1f6e64021f8ef703fc377b143cccb31abff28d66
lost-melody <lost__melody@163.com>
2023-09-28 22:18:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hamster Tools
+
+> [倉輸入法](https://github.com/imfuxiao/Hamster) 佈局生成器,
+> 基於 [Svelte](https://svelte.dev), [SvelteUI](https://svelteui.org) 構建.
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import App from './App.svelte'
+
+let element = document.getElementById('app');
+if (element === null) {
+    throw new Error("failed to get element by id");
+}
+
+const app = new App({
+    target: element,
+})
+
+export default app
